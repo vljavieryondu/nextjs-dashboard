22f26a3881037f7157d1dd07c7899e49bb57ffbe
Van Laurence Javier <vljavier@YONDU105121.local>
2025-07-05 10:55:09 +0800

    test throw not logged in

--- auth.config.ts
+++ auth.config.ts
@@ -7,9 +7,11 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
+
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
+        else throw new Error('isLoggedIn false');
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl));
