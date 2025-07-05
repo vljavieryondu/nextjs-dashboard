adf076b26c8e3fd543c925499611ab111f4dbcc3
Van Laurence Javier <vljavier@YONDU105121.local>
2025-07-05 10:59:10 +0800

    test throw error sign in success

--- auth.config.ts
+++ auth.config.ts
@@ -10,8 +10,7 @@ export const authConfig = {
 
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        else throw new Error('isLoggedIn false');
+        if (isLoggedIn) throw new Error("Success sign in");
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl));
