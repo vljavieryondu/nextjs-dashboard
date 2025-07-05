8f37ee84e01969fbff65807d8a4b938e25ddb502
Van Laurence Javier <vljavier@YONDU105121.local>
2025-07-05 11:13:29 +0800

    remove throw error

--- auth.config.ts
+++ auth.config.ts
@@ -10,7 +10,7 @@ export const authConfig = {
 
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
-        if (isLoggedIn) throw new Error("Success sign in");
+        if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl));
